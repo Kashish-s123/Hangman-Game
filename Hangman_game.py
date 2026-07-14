492810c4839a9c34c0a98fc324bf97c43180dc1f
Kashish-s123 <hemasingh3939@gmail.com>
2026-07-14 17:25:44 +0530

    Add Hangman Game project
    Uploaded Python Hangman Game for CodeAlpha Internship Task 1.
    Features:
    - Random word selection
    - 6 chances for guessing
    - Win/Lose conditions
    - Console-based gameplay

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,44 +1,44 @@
 import random
 
-word_list = ["python", "college", "laptop", "coding", "student"]
+word_list = ["kanpur", "python", "django", "cricket", "mobile"]
 
 secret_word = random.choice(word_list)
 
 guessed_letters = ""
 chances = 6
 
-print("===== HANGMAN GAME =====")
+print(" ============ Hangman Game by Kashish ============")
 print("Guess the word one letter at a time")
 
-while chances > 0:
+while chances > 0:   # 6 > 0
 
     display_word = ""
 
-    for letter in secret_word:
-        if letter in guessed_letters:
+    for letter in secret_word:    
+        if letter in guessed_letters:     
             display_word += letter + " "
         else:
-            display_word += "_ "
-
+            display_word += "_ "   
     print("\nWord:", display_word)
 
-    if "_" not in display_word:
+    if "_" not in display_word:   
         print("\nYou Won!")
         print("The word was:", secret_word)
         break
 
-    guess = input("Enter a letter: ").lower()
+    guess = input("Enter a letter: ").lower()     
 
-    if guess in guessed_letters:
+    if guess in guessed_letters:  
         print("You already guessed this letter.")
         continue
 
-    guessed_letters += guess
+    guessed_letters += guess   
 
-    if guess not in secret_word:
+    if guess not in secret_word:   
         chances -= 1
         print("Wrong Guess!")
         print("Remaining Chances:", chances)
+
 
 if chances == 0:
     print("\nGame Over!")
